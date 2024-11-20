b2da8070769f23806f7ab9d18fc4a4f8735df6fd
Peter Suh <85427533+PeterSuh-Q3@users.noreply.github.com>
2024-11-20 21:29:19 +0900

    Update jelly.sh

--- jelly.sh
+++ jelly.sh
@@ -6,6 +6,6 @@ mkdir -p /volume1/docker/jellyfin/cache
 curl -kLO# https://github.com/jellyfin/jellyfin-ffmpeg/releases/download/v6.0.1-8/jellyfin-ffmpeg6_6.0.1-8-bookworm_amd64.deb
 dpkg-deb -x jellyfin-ffmpeg*.deb temp_dir
 mkdir -p /usr/lib/jellyfin-ffmpeg
-mv -fv temp_dir/usr/lib/jellyfin-ffmpeg/ /usr/lib/
+rsync -av temp_dir/usr/lib/jellyfin-ffmpeg/ /usr/lib/jellyfin-ffmpeg/
 cd /usr/lib/jellyfin-ffmpeg
 ./ffmpeg
